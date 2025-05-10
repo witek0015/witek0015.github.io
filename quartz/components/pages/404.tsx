fce97aaea40f4b281abb19765487ecae91b05fe8
Wiktor <wiktor.czyzow@gmail.com>
2025-05-10 21:59:35 +0200

    Quartz sync: May 10, 2025, 9:59 PM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -7,8 +7,18 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   const baseDir = url.pathname
 
   return (
-    <article class="popover-hint">
-      <h1>404</h1>
+    <article class="popover-hint" style="text-align: center;">
+      <h1 style="text-align: center;"> 404</h1>
+      <img
+      src="https://witek0015.github.io/dimea-lore/static/404.png"
+      alt="Nie znaleziono strony"
+      style="
+      max-width: 300px;
+      margin-bottom: 1rem;
+      border-radius: 0.5rem;
+      border: 2px solid var(--dark);
+  "
+  />
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
